ce7a2793c54ad72eb4ba8163c385516dc2ae93bb
MadhuraManwatkar20 <madhuramanwatkar20@gmail.com>
2025-07-16 13:02:17 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+      <div className="container mt-5">
+      <h1 className="text-primary">Hello Bootstrap in React!</h1>
+      <button className="btn btn-success">Click Me</button>
+    </div> 
+  )}
+
